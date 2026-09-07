fa63023984686d5fefd19cadbf34216c7387db52
DOUMBOUYA Séré Moussa <dseremoussa@gmail.com>
2026-09-08 00:56:34 +0200

    correction des origins

--- src/main/java/friasoft/gn/schoolapp/security/ConfigurationSecurityApplication.java
+++ src/main/java/friasoft/gn/schoolapp/security/ConfigurationSecurityApplication.java
@@ -98,7 +98,9 @@ public class ConfigurationSecurityApplication {
         configuration.setAllowedOriginPatterns(patterns);
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
+        configuration.setExposedHeaders(List.of("Authorization", "Content-Disposition"));
         configuration.setAllowCredentials(true);
+        configuration.setMaxAge(3600L);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
